20a124c56166f632694c5cd66c34c403f8bfc032
Luna <Kobali17@yandex.ru>
2021-02-22 21:05:45 +0300

    fix base url

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -46,7 +46,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: '/api',
+  baseUrl: 'http://api.lunara-news.students.nomoredomains.icu',
   headers: {
     'Content-Type': 'application/json',
   },
